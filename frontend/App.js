629609e499978cb8de1b7916b8e8a716bfcd329d
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2024-02-04 21:50:26 +0700

    first upload

---
+++ frontend/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext"; //access token
+import ThemeProvider from "./theme";
+
+//
+function App() {
+  //
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
